ff7628934488e6029e7dec4d1b6398d859e51986
Julien Abbadie <julien.abbadie@pytheascapital.com>
2024-07-01 21:31:00 +0200

    remove csp

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: *.onrewind.tv *.axept.io http://nxtck.com/act.php https://apis.google.com/ cdn.tagcommander.com www.datadoghq-browser-agent.com connect.facebook.net www.googletagmanager.com www.googleadservices.com googleads.g.doubleclick.net cdn.jwplayer.com ssl.p.jwpcdn.com www.gstatic.com gum.criteo.com sync.commander1.com www.facebook.com http://ib.adnxs.com/getuid http://dsp.adfarm1.adition.com;
-  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
-  img-src * blob: data:;
-  media-src 'self' blob: data: *.onrewind.tv;
-  connect-src *;
-  font-src 'self' assets.onrewind.tv fonts.gstatic.com;
-  frame-src  https://js.stripe.com/ https://poc-origins.firebaseapp.com/__/auth/iframe;
-`;
-
 const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
