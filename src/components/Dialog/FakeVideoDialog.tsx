8e7a3969289c1c3511e5b984f52ff8acdfdc999f
Julien Abbadie <julien.abbadie@pytheascapital.com>
2024-07-03 09:20:42 +0200

    dialog text update

--- src/components/Dialog/FakeVideoDialog.tsx
+++ src/components/Dialog/FakeVideoDialog.tsx
@@ -8,24 +8,33 @@ import { useRouter } from 'next/router';
 
 import Player from '$components/Player';
 
+const DEFAULT_TEXT =
+  'Christian Horner has given his view on the dramatic late-race collision between Max Verstappen and Lando Norris in the Austrian Grand Prix, with the Red Bull boss arguing that the 10-second penalty handed to Verstappen was “harsh”.\n' +
+  '\n' +
+  'Verstappen had led for much of the race before seeing his advantage over second-placed Norris cut following a rare slow pit stop from the Red Bull squad on Lap 52 of 71. A thrilling chase ensued in the laps that followed as Norris attempted several overtakes in a battle for the lead.\n' +
+  '\n' +
+  'It all came to a head on Lap 64 when there was contact between the pair, resulting in both picking up punctures and having to return slowly to the pits. While Norris sustained too much damage to continue, Verstappen rejoined and ended the event in fifth place.' +
+  'The Dutchman did, however, receive a 10-second time penalty after being deemed at fault for the incident, but ultimately had enough of a gap to Nico Hulkenberg in sixth to hold the position.\n' +
+  '\n';
+
 const playlist = [
   {
     name: 'Christian Horner',
     duration: '1:30',
     views: 100,
-    text: "In a heated aftermath of the Austrian Grand Prix, Christian Horner defended Max Verstappen after his late-race collision with Lando Norris, labeling the 10-second penalty as “harsh.” Verstappen had been dominating the race until a rare slow pit stop on Lap 52 allowed Norris to close in. This set the stage for a thrilling duel between the two. On Lap 64, the tension peaked as contact between their cars caused punctures for both, forcing them to pit. Norris's car suffered extensive damage and he had to retire, while Verstappen managed to continue and finished fifth. Despite the penalty, he maintained a significant gap over Nico Hulkenberg, securing his position.",
+    text: DEFAULT_TEXT,
   },
   {
     name: 'Grand Prix Italie',
     duration: '1:30',
     views: 100,
-    text: 'Christian Horner has spoken out about the dramatic clash between Max Verstappen and Lando Norris during the Austrian Grand Prix, describing the 10-second penalty given to Verstappen as “harsh.” Verstappen had been leading comfortably until a slow pit stop on Lap 52 cut his lead. This allowed Norris to mount a challenge, leading to several intense overtaking attempts. The conflict reached its climax on Lap 64 with contact that resulted in punctures for both drivers. While Norris’s car was too damaged to continue, Verstappen was able to rejoin the race and finished in fifth place. Despite the penalty, he stayed ahead of Nico Hulkenberg, retaining his position.',
+    text: 'Christian Horner has spoken out about the dramatic clash between Max Verstappen and Lando Norris during the Austrian Grand Prix, describing the 10-second penalty given to Verstappen as “harsh.” \nVerstappen had been leading comfortably until a slow pit stop on Lap 52 cut his lead. \n\nThis allowed Norris to mount a challenge, leading to several intense overtaking attempts. \nThe conflict reached its climax on Lap 64 with contact that resulted in punctures for both drivers. While Norris’s car was too damaged to continue, Verstappen was able to rejoin the race and finished in fifth place. Despite the penalty, he stayed ahead of Nico Hulkenberg, retaining his position.',
   },
   {
     name: 'Austrian Grand Prix',
     duration: '1:30',
     views: 100,
-    text: 'Christian Horner has spoken out about the dramatic clash between Max Verstappen and Lando Norris during the Austrian Grand Prix, describing the 10-second penalty given to Verstappen as “harsh.” Verstappen had been leading comfortably until a slow pit stop on Lap 52 cut his lead. This allowed Norris to mount a challenge, leading to several intense overtaking attempts. The conflict reached its climax on Lap 64 with contact that resulted in punctures for both drivers. While Norris’s car was too damaged to continue, Verstappen was able to rejoin the race and finished in fifth place. Despite the penalty, he stayed ahead of Nico Hulkenberg, retaining his position.',
+    text: 'Christian Horner has spoken out about the dramatic clash between Max Verstappen and Lando Norris during the Austrian Grand Prix, describing the 10-second penalty given to Verstappen as “harsh.” \n\nVerstappen had been leading comfortably until a slow pit stop on Lap 52 cut his lead. This allowed Norris to mount a challenge, leading to several intense overtaking attempts. The conflict reached its climax on Lap 64 with contact that resulted in punctures for both drivers. While Norris’s car was too damaged to continue, Verstappen was able to rejoin the race and finished in fifth place. Despite the penalty, he stayed ahead of Nico Hulkenberg, retaining his position.',
   },
 ];
 
@@ -134,17 +143,10 @@ export default function FakeVideoDialog() {
           </div>
         </div>
 
-        <p className={'text-xs flex flex-col whitespace-pre-wrap font-normal'}>{DEFAULT_TEXT}</p>
+        <p className={'text-xs flex flex-col whitespace-pre-wrap font-normal'}>
+          {playlist[selected].text}
+        </p>
       </motion.div>
     </motion.div>
   );
 }
-
-const DEFAULT_TEXT =
-  'Christian Horner has given his view on the dramatic late-race collision between Max Verstappen and Lando Norris in the Austrian Grand Prix, with the Red Bull boss arguing that the 10-second penalty handed to Verstappen was “harsh”.\n' +
-  '\n' +
-  'Verstappen had led for much of the race before seeing his advantage over second-placed Norris cut following a rare slow pit stop from the Red Bull squad on Lap 52 of 71. A thrilling chase ensued in the laps that followed as Norris attempted several overtakes in a battle for the lead.\n' +
-  '\n' +
-  'It all came to a head on Lap 64 when there was contact between the pair, resulting in both picking up punctures and having to return slowly to the pits. While Norris sustained too much damage to continue, Verstappen rejoined and ended the event in fifth place.' +
-  'The Dutchman did, however, receive a 10-second time penalty after being deemed at fault for the incident, but ultimately had enough of a gap to Nico Hulkenberg in sixth to hold the position.\n' +
-  '\n';
